fefc014482d7b0153879d26485679a9fd98e6304
unknown <serpens8888@github.com>
2025-10-29 23:52:21 -0400

    make supermaven complete shift tab bc I need to indent sometimes

--- init.lua
+++ init.lua
@@ -4,3 +4,5 @@ require("config.keymaps")
 require("config.lazy")
 require("config.options")
 set_colorscheme()
+
+
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -4,6 +4,9 @@ return {
     config = function()
         require("supermaven-nvim").setup({
             disable_inline_completion = false, -- disables inline completion for use with cmp
+            keymaps = {
+                accept_suggestion = "<S-Tab>",
+            },
         })
     end,
 }
